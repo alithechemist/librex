4405fe4f87da4ac220c8713594f9230030be409e
Chippy <chippy@classictetris.net>
2023-11-17 13:47:52 +0100

    better...

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     <title>LibreX For Icecat</title>
     </head>
     <body>
-	<h3><a href="https://icecatbrowser.org">Icecat browser</a></h3>
         <form class="search-container" action="search.php" method="get" autocomplete="off">
                 <h1>Libre<span class="X">X</span></h1>
                 <input type="text" name="q" autofocus/>
@@ -13,7 +12,8 @@
                 <div class="search-button-wrapper">
                     <button name="t" value="0" type="submit">Search with LibreX</button>
                     <button name="t" value="3" type="submit">Search torrents with LibreX</button>
-                </div>
+		</div>
+		<a href="https://icecatbrowser.org">Download GNU Icecat for your PC</a>
         </form>
 
 <?php require "misc/footer.php"; ?>
